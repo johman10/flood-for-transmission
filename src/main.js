5fb2fc504bbc43075316cb5b2d794ac02a31916f
Johan van Eck <johveck@gmail.com>
2020-12-26 18:35:56 +0200

    Put back SW initialization

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import App from '~components/App';
 
-// if ('serviceWorker' in navigator) {
-//   navigator.serviceWorker.register('./sw.js', {
-//     scope: window.location.pathname,
-//   });
-// }
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./sw.js', {
+    scope: window.location.pathname,
+  });
+}
 
 new App({
   target: document.body,

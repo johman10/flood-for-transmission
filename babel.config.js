71466a561fcbf91d65b3fdb4b0d02b16e78b2296
Johan van Eck <johveck@gmail.com>
2020-12-26 18:35:29 +0200

    Update babel config for better browser support

--- babel.config.js
+++ babel.config.js
@@ -5,6 +5,7 @@ module.exports = {
       {
         useBuiltIns: 'usage',
         corejs: 3,
+        targets: 'last 2 Chrome versions, last 2 Firefox versions, last 2 Edge versions',
       },
     ],
   ],

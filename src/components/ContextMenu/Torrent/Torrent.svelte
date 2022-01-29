e48280d1ab8dde24cc7a19f589a24e5b241901bd
Johan van Eck <johveck@gmail.com>
2022-01-29 11:42:42 +0200

    Add option to copy magnet link to context menu of torrent

--- src/components/ContextMenu/Torrent/Torrent.svelte
+++ src/components/ContextMenu/Torrent/Torrent.svelte
@@ -8,6 +8,7 @@
   export let onStop;
   export let onRemove;
   export let onVerify;
+  export let onCopyMagnetLink;
   export let onAsk;
   export let onLabels;
   export let onLocation;
@@ -25,6 +26,9 @@
 <li on:click="{(...args) => onItemClick(onStop, args)}">Stop</li>
 <li on:click="{(...args) => onItemClick(onRemove, args)}">Remove</li>
 <li on:click="{(...args) => onItemClick(onVerify, args)}">Verify local data</li>
+<li on:click="{(...args) => onItemClick(onCopyMagnetLink, args)}">
+  Copy magnet link
+</li>
 <li on:click="{(...args) => onItemClick(onAsk, args)}">
   Ask tracker for more peers
 </li>

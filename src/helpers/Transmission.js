8d2cc2720c3650ccb34627f29cf392fcda76113e
Johan <johman10@users.noreply.github.com>
2023-02-03 18:09:31 +0200

    Add dropzone to list (#509)
    This allows to drop files anywhere on the torrent list to add them to
    the queue.
    
    Resolves: #414

--- src/helpers/Transmission.js
+++ src/helpers/Transmission.js
@@ -41,7 +41,7 @@ export default class Transmission {
       method: 'post',
       headers,
     });
-    this._tokenHeader = response.headers.get('X-Transmission-Session-Id');
+    this._tokenHeader = response.headers.get(TOKEN_HEADER);
   }
 
   async rpcCall(method, args = {}) {

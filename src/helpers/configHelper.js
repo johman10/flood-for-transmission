1710f012e0142aedbd38d468a266280f6b9a9111
defkev <defkev@gmail.com>
2023-02-17 12:48:24 +0100

    Improve handling of config.json defaults Instead of spreading each hard-coded default across all stores define defaults in configHelper and merge them with config.json on init

--- src/helpers/configHelper.js
+++ src/helpers/configHelper.js
@@ -1,11 +1,33 @@
+const defaults = {
+    DARK_MODE: 'auto',          // String (enabled, disabled, auto)
+    SWITCH_COLORS: false,       // Boolean
+    NOTATION_24H: true,         // Boolean
+    WRAP_HEADER: false,         // Boolean
+    COMMON_PATH: [],            // Array of Strings
+    COLUMNS: [                  // Array of Strings in the order they should appear
+        'Name',
+        'Progress',
+        'ETA',
+        'Download Speed',
+        'Upload Speed',
+        'File Size',
+        'Downloaded',
+        'Uploaded',
+        'Downloading from',
+        'Seeding to'
+    ],
+    SORT_COLUMN: 'Progress',    // String
+    SORT_DIRECTION: 'desc'      // String (asc, desc)
+}
+
 export default await fetch('./config.json')
     .then((res) => {
         return res.json();
     })
     .then((config) => {
-        return config;
+        return {...defaults, ...config};
     })
     .catch((e) => {
         console.error('Something went wrong while fetching config.json', e);
-        return {};
+        return defaults;
     });
--- src/helpers/stores/paths.js
+++ src/helpers/stores/paths.js
@@ -6,7 +6,7 @@ const PATHS_STORAGE_KEY = 'paths';
 function getPaths() {
   const storedPaths = window.localStorage.getItem(PATHS_STORAGE_KEY);
   if (storedPaths) return JSON.parse(storedPaths);
-  return config.COMMON_PATH || []
+  return config.COMMON_PATH
 }
 
 function cleanValue(value) {

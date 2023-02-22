b2757a4e22178f450af5389b8e9b30171b3a8b8e
defkev <defkev@gmail.com>
2023-02-22 06:06:57 +0100

    Revert config defaults to inline with nullish coalescing Revert remaining paths async stuff

--- src/helpers/stores/sorting.js
+++ src/helpers/stores/sorting.js
@@ -1,12 +1,13 @@
 import config from '~helpers/configHelper';
 import { writable } from 'svelte/store';
 import { UI_COLUMN } from '~helpers/constants/columns';
+import { UI_COLUMN_PROGRESS_BAR } from '~helpers/constants/columns';
 import { uiColumns } from '~helpers/stores';
 
 const SORTING_STORAGE_KEY = 'torrent-sorting';
 const DEFAULT_SORTING = {
-  id: Object.values(UI_COLUMN).find(column => column.label === config.SORT_COLUMN).id,
-  direction: config.SORT_DIRECTION,
+  id: (Object.values(UI_COLUMN).find(column => column.label === config.SORT_COLUMN) ?? UI_COLUMN_PROGRESS_BAR).id,
+  direction: config.SORT_DIRECTION ?? 'desc',
 };
 
 function storeSorting(value) {

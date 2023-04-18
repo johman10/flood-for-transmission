6f092edf179dec9f1f7656a95629e4cfbed10edb
Johan van Eck <johveck@gmail.com>
2023-04-18 20:39:48 +0300

    Fix overflow due to dropzone

--- src/components/TorrentList/TorrentDropzone.svelte
+++ src/components/TorrentList/TorrentDropzone.svelte
@@ -113,17 +113,14 @@
     width: 100%;
     height: 100%;
     inset: 0;
-    opacity: 0;
     background-color: var(--color-dropzone-background);
-    pointer-events: none;
-    display: flex;
+    display: none;
     align-items: center;
     justify-content: center;
   }
 
   .dropzone.hovering {
-    opacity: 1;
-    pointer-events: all;
+    display: flex;
   }
 
   .dropzone:before {

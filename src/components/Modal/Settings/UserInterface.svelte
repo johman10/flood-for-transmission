52777f5168b26fe2d812c65b1e71680d2e555fd5
Johan van Eck <johveck@gmail.com>
2024-01-24 18:12:52 +0200

    Fix path pattern regex

--- src/components/Modal/Settings/UserInterface.svelte
+++ src/components/Modal/Settings/UserInterface.svelte
@@ -15,6 +15,7 @@
     tableHeaderConfig,
   } from '~helpers/stores';
   import { orderable } from '~helpers/actions';
+  import { PATH_VALIDATION_REGEX } from '~helpers/constants/paths';
 
   const newColumns = JSON.parse(JSON.stringify($uiColumns));
   let newPaths = [...$paths];
@@ -96,7 +97,7 @@
   </p>
   <InputMultiple
     bind:values="{newPaths}"
-    pattern="^/.*"
+    pattern="{PATH_VALIDATION_REGEX}"
     validationMessage="Path must be an absolute path."
   />
 

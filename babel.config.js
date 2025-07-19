b92df702f63108adc6619967f136140431ff83e9
Johan van Eck <johveck@gmail.com>
2025-07-19 11:18:45 +0200

    Update dependencies

--- babel.config.js
+++ babel.config.js
@@ -5,9 +5,13 @@ module.exports = {
       {
         useBuiltIns: 'usage',
         corejs: 3,
-        targets: 'last 2 Chrome versions, last 2 Firefox versions, last 2 Edge versions',
+        targets:
+          'last 2 Chrome versions, last 2 Firefox versions, last 2 Edge versions',
       },
     ],
   ],
-  plugins: ['@babel/plugin-proposal-class-properties'],
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-transform-private-methods',
+  ],
 };
